581d0e944324c8820fc45b10863a0bd688c9522f
Bruce Frederiksen <dangyogi@gmail.com>
2026-03-15 12:14:15 -0400

    Reconcile.csv loaded with most Revenue, Sam's Club expenses:
      - missing $20 from Paul for adv tickets
      - missing meeting dinner expenses
      - missing Walmart expenses
      - missing 120 Club donation

--- csv_beans/update_reconcile.py
+++ csv_beans/update_reconcile.py
@@ -25,9 +25,50 @@ def run():
     args = parser.parse_args()
 
     load_database()
+    last_row = Reconcile[-1]
+    if last_row.account == "cash" and last_row.detail == "w/starts":
+        starting_balance = last_row
+        ending_balance = starting_balance.copy()
+    else:
+        starting_balance = None
+    last_date = last_row.date
+    starting_num_rows = len(Reconcile)
     recon_file = args.reconcile_csv_file or "Reconcile.csv"
     print("Copying", recon_file, "into database")
-    load_csv(recon_file, from_scratch=False)
+    rows_added = load_csv(recon_file, from_scratch=False)
+    index = Reconcile.find_date(last_date, find_first=False)
+    assert index == starting_num_rows, \
+      f"Reconcile started with {starting_num_rows} rows up to {last_date}, now has {index} rows up to that date"
+    date_column = Reconcile.row_class.column_map['date']
+    total = 0
+    for row in Reconcile[index:]:
+        print(f"{date_column.to_csv(row.date)}: {row.account}({row.detail}) = {row.total}", end='')
+        if row.donations:
+            print(f", donations={row.donations}")
+        else:
+            print()
+        if row.type == "Revenue":
+            total += row.total
+            if starting_balance is not None:
+                ending_balance += row
+                if (row.account, "start") in Starts:
+                    ending_balance -= Starts[row.account, "start"]
+        elif row.type == "Expenses":
+            total -= row.total
+            if starting_balance is not None:
+                ending_balance -= row
+    print("total", total)
+    if starting_balance is not None:
+        print("ending balance|coin|b1|b5|b10|b20|b50|b100|total")
+        print("              ", end='')
+        print(f"|{ending_balance.coin:4.3}", end='')
+        print(f"|{ending_balance.b1:2}", end='')
+        print(f"|{ending_balance.b5:2}", end='')
+        print(f"|{ending_balance.b10:3}", end='')
+        print(f"|{ending_balance.b20:3}", end='')
+        print(f"|{ending_balance.b50:3}", end='')
+        print(f"|{ending_balance.b100:4}", end='')
+        print(f"|{ending_balance.total}")
 
     if args.trial_run:
         print("Trial_run: Database not saved")
